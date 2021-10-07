d65e0a67f4b16be63df05ba610a3d4fd0626af19
RIU-TGW <riu@tgw.at>
2021-10-07 21:24:39 +0200

    Formatierung

--- src/geo/Geometrics.java
+++ src/geo/Geometrics.java
@@ -10,57 +10,59 @@ public class Geometrics {
 
 
     public static void main(String[] args) {
-        squareAreaCalc();
-        squarePerimeterCalc();
+        
+        System.out.println();
+        System.out.println("Area calculation");
+        squareAreaCalc();        
         rectangleAreaCalc();
+
+        System.out.println();
+        System.out.println("Perimeter calculation");
+        squarePerimeterCalc();
         rectanglePerimeterCalc();
         
-        System.out.println("We can also calculate some volumes");
+        System.out.println();
+        System.out.println("Volumes calculation");
         zoneSphere();
         sphereCylinder();
-        ungula();        
+        ungula();  
+        
+        System.out.println();
     }
 
     public static void squareAreaCalc() {
-        System.out.println();
-        System.out.println("Square area");
-        System.out.println("a * a = " + (a * a)); 
+        System.out.println("    Square area:");
+        System.out.println("        a * a = " + (a * a)); 
     }
 
     public static void squarePerimeterCalc() {
-        System.out.println();
-        System.out.println("Square perimeter");
-        System.out.println("4 * a = " + (4 * a));
+        System.out.println("    Square perimeter:");
+        System.out.println("        4 * a = " + (4 * a));
     }
 
     public static void rectangleAreaCalc() {
-        System.out.println();
-        System.out.println("Rectangle area");
-        System.out.println("a * b = " + (a * b));
+        System.out.println("    Rectangle area:");
+        System.out.println("        a * b = " + (a * b));
     }
 
     public static void rectanglePerimeterCalc() {
-        System.out.println();
-        System.out.println("Rectangle perimeter");
-        System.out.println("a + b = " + (a + b));
+        System.out.println("    Rectangle perimeter:");
+        System.out.println("        a + b = " + (a + b));
     }
 
     public static void zoneSphere() {
-        System.out.println();
-        System.out.println("Zone of a sphere");
-        System.out.println("V = " + ((Math.PI * h * (3 * Math.pow(r, 2) + 3 * Math.pow(x, 2) + Math.pow(h, 2))) / 6));
+        System.out.println("    Zone of a sphere:");
+        System.out.println("        V = " + ((Math.PI * h * (3 * Math.pow(r, 2) + 3 * Math.pow(x, 2) + Math.pow(h, 2))) / 6));
     }
 
     public static void sphereCylinder() {
-        System.out.println();
-        System.out.println("Sphere with cylinder");
-        System.out.println("V = " + ((Math.PI * Math.pow(h, 3)) / 6)); 
+        System.out.println("    Sphere with cylinder:");
+        System.out.println("        V = " + ((Math.PI * Math.pow(h, 3)) / 6)); 
     }
 
     public static void ungula() {
-        System.out.println();
-        System.out.println("Ungula");
-        System.out.println("V = " + ((2 * Math.pow(r, 2) * h) / 3));
+        System.out.println("    Ungula:");
+        System.out.println("        V = " + ((2 * Math.pow(r, 2) * h) / 3));
     }
 
 }
