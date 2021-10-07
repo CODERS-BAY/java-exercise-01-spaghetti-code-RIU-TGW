d70cdf3d0119ccd29596f20d29694c2e59164fd6
RIU-TGW <riu@tgw.at>
2021-10-07 20:57:17 +0200

    Fixed bad spaghetti cod

--- src/geo/Geometrics.java
+++ src/geo/Geometrics.java
@@ -1,58 +1,66 @@
 package geo;
 public class Geometrics {
-    private static double x = 10.0;
-    private static double y = 5.0;
-    private static double z = 3.0;
-    private static int x1 = 8;
-    private static int y1 = 7;
-    private static int z1 = 10;
-    private static int x2 = 100;
-    private static int y2 = 57;
-    private static int z2 = 129;
-    private static int x3 = 45;
-    private static int y3 = 89;
-    private static int z3 = 69;
+    private static double a = 10.0;
+    private static double b = 5.0;
+    private static double c = 3.0;
+
+    private static double h = 5.0;
+    private static double r = 3.0;
+    private static double x = 4.0;
+
 
     public static void main(String[] args) {
-        // area ... Fläche
-        System.out.println("Square area");
-        System.out.println("x * x = " + x * x);
-        System.out.println("x1 * x1 = " + x1 * x1);
-        System.out.println("x2 * x2 = " + x2 * x2);
-        System.out.println("y * y = " + y * y);
-
-        // perimeter ... Umfang
-        System.out.println("Square perimeter");
-        System.out.println("4 * x = " + 4 * x);
-        System.out.println("4 * x1 = " + 4 * x1);
-        System.out.println("4 * x2 = " + 4 * x2);
-        System.out.println("4 * y = " + 4 * y);
-
-        System.out.println("Rectangle area");
-        System.out.println("x * y = " + x * y);
-        System.out.println("x1 * y1 = " + x1 * y1);
-        System.out.println("x2 * y2 = " + x2 * y2);
-        System.out.println("x3 * y3 = " + x3 * y3);
-
-        System.out.println("Rectangle perimeter");
-        System.out.println("x + y = " + x + y);
-        System.out.println("x1 + y1 = " + x1 + y1);
-        System.out.println("x2 + y2 = " + x2 + y2);
-        System.out.println("x3 + y3 = " + x3 + y3);
-
+        squareAreaCalc();
+        squarePerimeterCalc();
+        rectangleAreaCalc();
+        rectanglePerimeterCalc();
+        
         System.out.println("We can also calculate some volumes");
-        /*
-            z is our height
-            y is our radius
-         */
-        System.out.println("Zone of a sphere");
-        double v = (Math.PI * z * (3 * Math.pow(y, 2) + 3 * Math.pow(x, 2) + Math.pow(z, 2))) / 6;
-        System.out.println(v);
-
-        System.out.println("Sphere with cylinder");
-        System.out.println(Math.PI * Math.pow(z, 3) / 6);
-
-        System.out.println("Ungula");
-        System.out.println((double) (2 * x3 * z3) / 3);
+        zoneSphere();
+        sphereCylinder();
+        ungula();        
     }
+
+    public static void squareAreaCalc() {
+        System.out.println();
+        System.out.println("Square area");
+        System.out.println("a * a = " + (a * a)); 
+    }
+
+    public static void squarePerimeterCalc() {
+        System.out.println();
+        System.out.println("Square perimeter");
+        System.out.println("4 * a = " + (4 * a));
+    }
+
+    public static void rectangleAreaCalc() {
+        System.out.println();
+        System.out.println("Rectangle area");
+        System.out.println("a * b = " + (a * b));
+    }
+
+    public static void rectanglePerimeterCalc() {
+        System.out.println();
+        System.out.println("Rectangle perimeter");
+        System.out.println("a + b = " + (a + b));
+    }
+
+    public static void zoneSphere() {
+        System.out.println();
+        System.out.println("Zone of a sphere");
+        System.out.println("V = " + ((Math.PI * h * (3 * Math.pow(r, 2) + 3 * Math.pow(x, 2) + Math.pow(h, 2))) / 6));
+    }
+
+    public static void sphereCylinder() {
+        System.out.println();
+        System.out.println("Sphere with cylinder");
+        System.out.println("V = " + ((Math.PI * Math.pow(h, 3)) / 6)); 
+    }
+
+    public static void ungula() {
+        System.out.println();
+        System.out.println("Ungula");
+        System.out.println("V = " + ((2 * Math.pow(r, 2) * h) / 3));
+    }
+
 }
